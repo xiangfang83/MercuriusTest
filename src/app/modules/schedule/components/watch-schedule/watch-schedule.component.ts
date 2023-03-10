5a94c7ebc69e5c33d999904252b23212aab379b4
Xiang Fang <xiangfang83@gmail.com>
2023-03-09 18:02:18 -0600

    #feature: weekly view

--- src/app/modules/schedule/components/watch-schedule/watch-schedule.component.ts
+++ src/app/modules/schedule/components/watch-schedule/watch-schedule.component.ts
@@ -13,9 +13,41 @@ export class WatchScheduleComponent implements OnInit {
 
   viewMode: string;
   daily_data: object[] = daily_data_json;
+  weekly_time_data: string[] = [];
+  weekly_date_data: string[] = [];
+  weekly_table_data: object[][] = [];
 
   constructor() { 
     this.viewMode = "Daily";
+    weekly_data_json.forEach((data) => {
+      if (this.weekly_time_data.indexOf(data.time) === -1) {
+        this.weekly_time_data.push(data.time)
+      }
+    });
+    weekly_data_json.forEach((data) => {
+      if (this.weekly_date_data.indexOf(data.date) === -1) {
+        this.weekly_date_data.push(data.date)
+      }
+    });
+    this.weekly_time_data.forEach((time) => {
+      let tmpArr: object[] = [];
+      this.weekly_date_data.forEach((date) => {
+        let result = weekly_data_json.filter((data) => data.date === date && data.time === time);
+        if (result.length > 0) tmpArr.push(result[0]);
+        else tmpArr.push({
+          bidscheduleid: 0,
+          managerid: 0,
+          areaid: "",
+          date: date,
+          time: time,
+          year: "",
+          duration: 0,
+          empinfo: []
+        });
+      });
+      this.weekly_table_data.push(tmpArr);
+    })
+
   }
 
   ngOnInit() {
